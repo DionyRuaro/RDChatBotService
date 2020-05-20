3892d54c48023e3d8e88c88fad801829f766a089
Diony Ruaro <diony.ruaro@rduphold.com.br>
2020-05-20 09:31:40 -0300

    Create Project

---
+++ src/main/java/com/rd/RdChatBotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdChatBotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdChatBotServiceApplication.class, args);
+	}
+
+}
